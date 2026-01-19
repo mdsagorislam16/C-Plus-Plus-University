9d8062c535faac96b894e6a293344a5722301973
saagor16 <saagor.16@gmail.com>
2026-01-19 09:30:25 +0600

    all relational operators

---
+++ Basics_&_Data_Types/Relational_Operators.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    if (a == b)
+    {
+        cout << a << " is equal to " << b << endl;
+    }
+    else
+    {
+        cout << a << " is not equal to " << b << endl;
+    }
+
+    if (a != b)
+    {
+        cout << a << " is not equal to " << b << endl;
+    }
+    else
+    {
+        cout << a << " is equal to " << b << endl;
+    }
+
+    if (a > b)
+    {
+        cout << a << " is greater than " << b << endl;
+    }
+    else
+    {
+        cout << a << " is not greater than " << b << endl;
+    }
+    if (a < b)
+    {
+        cout << a << " is less than " << b << endl;
+    }
+    else
+    {
+        cout << a << " is not less than " << b << endl;
+    }
+    if (a >= b)
+    {
+        cout << a << " is greater than or equal to " << b << endl;
+    }
+    else
+    {
+        cout << a << " is less than " << b << endl;
+    }
+    if (a <= b)
+    {
+        cout << a << " is less than or equal to " << b << endl;
+    }
+    else
+    {
+        cout << a << " is greater than " << b << endl;
+    }
+    return 0;
+}
