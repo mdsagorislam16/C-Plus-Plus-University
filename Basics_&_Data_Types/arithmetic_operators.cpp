ffa44fe67c0bf64de03433f59a50b5d25a092c54
saagor16 <saagor.16@gmail.com>
2026-01-19 09:21:02 +0600

    arithmetic operators

---
+++ Basics_&_Data_Types/arithmetic_operators.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a = 10, b = 3;
+
+    cout << "Addition: " << a + b << endl;
+    cout << "Subtraction: " << a - b << endl;
+    cout << "Multiplication: " << a * b << endl;
+    cout << "Division: " << a / b << endl;
+    cout << "Modulus: " << a % b << endl;
+
+    cout << "Post Increment: " << a++ << endl;
+    cout << "After Post Increment: " << a << endl;
+
+    cout << "Pre Increment: " << ++b << endl;
+
+    return 0;
+}
