2e4b6415632cf2fd3c7298f95e7dafa45bf86825
saagor16 <saagor.16@gmail.com>
2026-01-19 09:16:52 +0600

    Initial commit: add C++ data type size program

---
+++ Basics_&_Data_Types/data_type_size_pgrm.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char a;
+    signed char b;
+    unsigned char c;
+
+    short d;
+    signed short e;
+    unsigned short f;
+
+    int g;
+    signed int h;
+    unsigned int i;
+
+    short int j;
+    signed short int k;
+    unsigned short int l;
+
+    long int m;
+    signed long int n;
+    unsigned long int o;
+
+    float p;
+    double q;
+    long double r;
+
+    cout << "char: " << sizeof(a) << " byte" << endl;
+    cout << "signed char: " << sizeof(b) << " byte" << endl;
+    cout << "unsigned char: " << sizeof(c) << " byte" << endl;
+
+    cout << "short: " << sizeof(d) << " byte" << endl;
+    cout << "signed short: " << sizeof(e) << " byte" << endl;
+    cout << "unsigned short: " << sizeof(f) << " byte" << endl;
+
+    cout << "int: " << sizeof(g) << " byte" << endl;
+    cout << "signed int: " << sizeof(h) << " byte" << endl;
+    cout << "unsigned int: " << sizeof(i) << " byte" << endl;
+
+    cout << "short int: " << sizeof(j) << " byte" << endl;
+    cout << "signed short int: " << sizeof(k) << " byte" << endl;
+    cout << "unsigned short int: " << sizeof(l) << " byte" << endl;
+
+    cout << "long int: " << sizeof(m) << " byte" << endl;
+    cout << "signed long int: " << sizeof(n) << " byte" << endl;
+    cout << "unsigned long int: " << sizeof(o) << " byte" << endl;
+
+    cout << "float: " << sizeof(p) << " byte" << endl;
+    cout << "double: " << sizeof(q) << " byte" << endl;
+    cout << "long double: " << sizeof(r) << " byte" << endl;
+
+    return 0;
+}
